ad57018e1ab296c1ba31a48485c6e8db428dbea4
Anthony Damico <ajdamico@gmail.com>
2017-10-17 18:46:50 -0400

    recreate from scratch every time

--- tests/setup.R
+++ tests/setup.R
@@ -9,7 +9,7 @@ atus_cat <-
 # sample 25% of the records
 which_records <- sample( seq( nrow( atus_cat ) ) , round( nrow( atus_cat ) * 0.25 ) )
 
-# always sample year == 2015
-atus_cat <- unique( rbind( atus_cat[ which_records , ] , subset( atus_cat , year == 2015 ) ) )
+# always sample directory == 2015
+atus_cat <- unique( rbind( atus_cat[ which_records , ] , subset( atus_cat , directory == 2015 ) ) )
 
 lodown( "atus" , atus_cat )
