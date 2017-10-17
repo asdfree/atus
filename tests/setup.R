d2e56ff2e6e14451e8f3bda39f486886749fbfb5
Anthony Damico <ajdamico@gmail.com>
2017-10-17 11:53:03 -0400

    atus reduction

--- tests/setup.R
+++ tests/setup.R
@@ -6,8 +6,8 @@ atus_cat <-
 	get_catalog( "atus" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 50% of the records
-which_records <- sample( seq( nrow( atus_cat ) ) , round( nrow( atus_cat ) * 0.50 ) )
+# sample 25% of the records
+which_records <- sample( seq( nrow( atus_cat ) ) , round( nrow( atus_cat ) * 0.25 ) )
 
 # always sample year == 2015
 atus_cat <- unique( rbind( atus_cat[ which_records , ] , subset( atus_cat , year == 2015 ) ) )
