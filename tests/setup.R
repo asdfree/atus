58851433d756f3dd7377c4cb3513667f42ffdaf9
Anthony Damico <ajdamico@gmail.com>
2017-11-12 03:13:09 -0500

    fixes https://github.com/ajdamico/asdfree/issues/308

--- tests/setup.R
+++ tests/setup.R
@@ -1,15 +1,15 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
 library(lodown)
 
 atus_cat <-
 	get_catalog( "atus" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 25% of the records
-which_records <- sample( seq( nrow( atus_cat ) ) , round( nrow( atus_cat ) * 0.25 ) )
+record_categories <- ceiling( seq( nrow( atus_cat ) ) / ceiling( nrow( atus_cat ) / 5 ) )
 
-# always sample directory == 2015
-atus_cat <- unique( rbind( atus_cat[ which_records , ] , subset( atus_cat , directory == 2015 ) ) )
+atus_cat <- unique( rbind( atus_cat[ record_categories == this_sample_break , ] , atus_cat[ atus_cat$directory == 2015 , ] ) )
 
 lodown( "atus" , atus_cat )
